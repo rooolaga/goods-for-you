4e02ca47811e1e176f91a28e60020b841bdedc3a
rooolaga <rooolaga@gmail.com>
2024-03-22 13:50:47 +0300

    Skeleton and QueryError

--- src/features/Catalog/ui/CatalogFilter.tsx
+++ src/features/Catalog/ui/CatalogFilter.tsx
@@ -3,10 +3,12 @@ import cls from './CatalogFilter.module.scss'
 import { RadioGroup } from "@/shared/ui/RadioGroup"
 import { useState } from "react"
 import { Button } from "@/shared/ui/Button"
+import { useGetAllCategoriesQuery } from "@/entities/Product";
+import { CatalogFilterSkeleton } from "@/features/Catalog/ui/CatalogFilterSkeleton";
 import clsx from "clsx"
 
-import { categories, title } from '@/data/CatalogFilter.json'
-import { useGetAllCategoriesQuery } from "@/entities/Product";
+import { title } from '@/data/CatalogFilter.json'
+import { QueryError } from "@/shared/ui/QueryError";
 
 export const CatalogFilter = () => {
   const [category, setCategory] = useState<string | null>(null);
@@ -15,14 +17,24 @@ export const CatalogFilter = () => {
     setCategory(value);
   }
 
+  if (categories.isLoading) return (
+    <div className={cls.filter}>
+      <CatalogFilterSkeleton/>
+    </div>
+  );
+
+  if (categories.error) return (
+    <QueryError error={categories.error}/>
+  );
+
   return (
     <div className={cls.filter}>
       <Text as='h3' weight="semibold" className={clsx(cls.title, "mb-7")}>
-        {title}
+        {title} {category}
       </Text>
-      { categories.data && (
-        <RadioGroup name="category" label="Category" options={categories.data} onChange={onCategoryChange}/>
-      )}
+
+      <RadioGroup name="category" label="Category" options={categories.data} onChange={onCategoryChange}/>
+
       <div className={cls.filter_contols}>
         <Button variant="dark" size='l'>Apply</Button>
         <Button variant="text" size='l'>Reset</Button>
